643502da52f9797fe591c306ca7483ff38fe249d
Kris Coppieters <kris@rorohiko.com>
2024-07-24 10:50:29 +1200

    Removed use of async/await

--- CreativeDeveloperTools_UXP/crdtuxp_test.js
+++ CreativeDeveloperTools_UXP/crdtuxp_test.js
@@ -12,13 +12,13 @@ async function testBase64() {
     let s = "Hello World☜✿\x00\x7Féøo";
     let s64 = await crdtuxp.base64encode(s);
     if (s64 != "SGVsbG8gV29ybGTimJzinL8Af8Opw7hv") {
-        await crdtuxp.logError(arguments, "failed to crdtuxp.base64encode()");
+        crdtuxp.logError(arguments, "failed to crdtuxp.base64encode()");
         retVal = false;
     }
 
     let sRoundTrip = await crdtuxp.base64decode(s64);
     if (s != sRoundTrip) {
-        await crdtuxp.logError(arguments, "failed to crdtuxp.base64decode()");
+        crdtuxp.logError(arguments, "failed to crdtuxp.base64decode()");
         retVal = false;
     }
 
@@ -32,7 +32,7 @@ async function testDirs() {
     let desktopDir = await crdtuxp.getDir(crdtuxp.DESKTOP_DIR);
     let desktopDirExists = await crdtuxp.dirExists(desktopDir);
     if (! desktopDirExists) {
-        await crdtuxp.logError(arguments, "failed to verify existence of desktop dir");
+        crdtuxp.logError(arguments, "failed to verify existence of desktop dir");
         retVal = false;
     }
 
@@ -40,7 +40,7 @@ async function testDirs() {
     let testDirPath = desktopDir + testDirName + "/";
     let testDirExists = await crdtuxp.dirExists(testDirPath);
     if (testDirExists) {
-        await crdtuxp.logError(arguments, "testDir unexpectedly exists");
+        crdtuxp.logError(arguments, "testDir unexpectedly exists");
         retVal = false;
     }
     else {
@@ -48,7 +48,7 @@ async function testDirs() {
         testDirExists = await crdtuxp.dirExists(testDirPath);
     }
     if (! testDirExists) {
-        await crdtuxp.logError(arguments, "testDir unexpectedly does not exists");
+        crdtuxp.logError(arguments, "testDir unexpectedly does not exists");
         retVal = false;
     }
 
@@ -91,23 +91,23 @@ async function testDirs() {
     await crdtuxp.fileClose(readFileHandle4);
 
     if (fileContentAsString != stringReadContent) {
-        await crdtuxp.logError(arguments, "failed to read file as string");
+        crdtuxp.logError(arguments, "failed to read file as string");
         retVal = false;
     }
 
     if (fileContentAsString != file2ReadContent) {
-        await crdtuxp.logError(arguments, "failed to read UTF8 file as string");
+        crdtuxp.logError(arguments, "failed to read UTF8 file as string");
         retVal = false;
     }
 
     if (fileContentAsString != file3ReadContent) {
-        await crdtuxp.logError(arguments, "failed to read appended file as string");
+        crdtuxp.logError(arguments, "failed to read appended file as string");
         retVal = false;
     }
 
     let alternateStringReadContent = await crdtuxp.binaryUTF8ToStr(binaryReadContent);
     if (fileContentAsString != alternateStringReadContent) {
-        await crdtuxp.logError(arguments, "failed to read file as binary then string");
+        crdtuxp.logError(arguments, "failed to read file as binary then string");
         retVal = false;
     }
 
@@ -119,37 +119,37 @@ async function testDirs() {
     }
 
     if (! foundTestFile) {
-        await crdtuxp.logError(arguments, "failed to find test file on Desktop");
+        crdtuxp.logError(arguments, "failed to find test file on Desktop");
         retVal = false;
     }
 
     let fileExists = await crdtuxp.fileExists(testFilePath);
     if (! fileExists) {
-        await crdtuxp.logError(arguments, "fileExists failed");
+        crdtuxp.logError(arguments, "fileExists failed");
         retVal = false;
     }
 
     let fileAsDirExists = await crdtuxp.dirExists(testFilePath);
     if (fileAsDirExists) {
-        await crdtuxp.logError(arguments, "dirExists should not return 'true' on a file (instead of a dir)");
+        crdtuxp.logError(arguments, "dirExists should not return 'true' on a file (instead of a dir)");
         retVal = false;
     }
 
     let success = await crdtuxp.fileDelete(testFilePath);
     if (! success) {
-        await crdtuxp.logError(arguments, "fileDelete should return true");
+        crdtuxp.logError(arguments, "fileDelete should return true");
         retVal = false;
     }
 
     let tryAgainSuccess = await crdtuxp.fileDelete(testFilePath);
     if (tryAgainSuccess) {
-        await crdtuxp.logError(arguments, "second fileDelete on same file should return false");
+        crdtuxp.logError(arguments, "second fileDelete on same file should return false");
         retVal = false;
     }
 
     let deletedFileExists = await crdtuxp.fileExists(testFilePath);
     if (deletedFileExists) {
-        await crdtuxp.logError(arguments, "file should not exist any more");
+        crdtuxp.logError(arguments, "file should not exist any more");
         retVal = false;
     }
 
@@ -158,7 +158,7 @@ async function testDirs() {
 
     testDirExists = await crdtuxp.dirExists(testDirPath);
     if (! testDirExists) {
-        await crdtuxp.logError(arguments, "testDir should not disappear as it is not empty");
+        crdtuxp.logError(arguments, "testDir should not disappear as it is not empty");
         retVal = false;
     }
 
@@ -167,7 +167,7 @@ async function testDirs() {
 
     testDirExists = await crdtuxp.dirExists(testDirPath);
     if (testDirExists) {
-        await crdtuxp.logError(arguments, "testDir should disappear with recursive delete");
+        crdtuxp.logError(arguments, "testDir should disappear with recursive delete");
         retVal = false;
     }
     
@@ -184,19 +184,19 @@ async function testEncrypt() {
     let s1 = await crdtuxp.encrypt(s, key);
     let s2 = await crdtuxp.encrypt(s, key);
     if (s1 == s2) {
-        await crdtuxp.logError(arguments, "Encrypting the same string twice should give a different result");
+        crdtuxp.logError(arguments, "Encrypting the same string twice should give a different result");
         retVal = false;
     }
 
     let sRoundTrip1 = await crdtuxp.decrypt(s1, key);
     if (sRoundTrip1 != s) {
-        await crdtuxp.logError(arguments, "failed to decrypt s1");
+        crdtuxp.logError(arguments, "failed to decrypt s1");
         retVal = false;
     }
 
     let sRoundTrip2 = await crdtuxp.decrypt(s2, key);
     if (sRoundTrip2 != s) {
-        await crdtuxp.logError(arguments, "failed to decrypt s2");
+        crdtuxp.logError(arguments, "failed to decrypt s2");
         retVal = false;
     }
 
@@ -210,7 +210,7 @@ async function testEnvironment() {
     let homeDir = await crdtuxp.getDir(crdtuxp.HOME_DIR);
     let homeDirExists = await crdtuxp.dirExists(homeDir);
     if (! homeDirExists) {
-        await crdtuxp.logError(arguments, "failed to verify existence of home dir");
+        crdtuxp.logError(arguments, "failed to verify existence of home dir");
         retVal = false;
     }
 
@@ -265,7 +265,7 @@ async function testEnvironment() {
     }
 
     if (! matchSucceeded) {
-        await crdtuxp.logError(arguments, "HOME_DIR " + homeDir + " does not match env. let " + environmentHomeDirVariableName + " = " + environmentHomeDir);
+        crdtuxp.logError(arguments, "HOME_DIR " + homeDir + " does not match env. let " + environmentHomeDirVariableName + " = " + environmentHomeDir);
         retVal = false;
     }
 
@@ -278,19 +278,19 @@ async function testIntPow() {
 
     let x = crdtuxp.intPow(2, 10);
     if (x != 1024) {
-        await crdtuxp.logError(arguments, "2^10 intPow failed");
+        crdtuxp.logError(arguments, "2^10 intPow failed");
         retVal = false;
     }
 
     x = crdtuxp.intPow(-2, 11);
     if (x != -2048) {
-        await crdtuxp.logError(arguments, "(-2)^11 intPow failed");
+        crdtuxp.logError(arguments, "(-2)^11 intPow failed");
         retVal = false;
     }
 
     x = crdtuxp.intPow(0,0);
     if (! isNaN(x)) {
-        await crdtuxp.logError(arguments, "0^0 intPow failed");
+        crdtuxp.logError(arguments, "0^0 intPow failed");
         retVal = false;
     }
 
@@ -304,42 +304,42 @@ async function testLeftRightPad() {
 
     let s = "1234567890";
     if (crdtuxp.rightPad(s, "x", 12) != s + "xx") {
-        await crdtuxp.logError(arguments, "rightPad extension fails");
+        crdtuxp.logError(arguments, "rightPad extension fails");
         retVal = false;
     }
 
     if (crdtuxp.rightPad(s, "x", 10) != s) {
-        await crdtuxp.logError(arguments, "rightPad non-extension fails");
+        crdtuxp.logError(arguments, "rightPad non-extension fails");
         retVal = false;
     }
 
     if (crdtuxp.rightPad(s, "x", 8) != s.substring(0, 8)) {
-        await crdtuxp.logError(arguments, "rightPad reduction fails");
+        crdtuxp.logError(arguments, "rightPad reduction fails");
         retVal = false;
     }
 
     if (crdtuxp.rightPad("", "x", 8) != "xxxxxxxx") {
-        await crdtuxp.logError(arguments, "rightPad empty fails");
+        crdtuxp.logError(arguments, "rightPad empty fails");
         retVal = false;
     }
 
     if (crdtuxp.leftPad(s, "x", 12) != "xx" + s) {
-        await crdtuxp.logError(arguments, "leftPad extension fails");
+        crdtuxp.logError(arguments, "leftPad extension fails");
         retVal = false;
     }
 
     if (crdtuxp.leftPad(s, "x", 10) != s) {
-        await crdtuxp.logError(arguments, "leftPad non-extension fails");
+        crdtuxp.logError(arguments, "leftPad non-extension fails");
         retVal = false;
     }
 
     if (crdtuxp.leftPad(s, "x", 8) != s.substring(2)) {
-        await crdtuxp.logError(arguments, "leftPad reduction fails");
+        crdtuxp.logError(arguments, "leftPad reduction fails");
         retVal = false;
     }
 
     if (crdtuxp.leftPad("", "x", 8) != "xxxxxxxx") {
-        await crdtuxp.logError(arguments, "leftPad empty fails");
+        crdtuxp.logError(arguments, "leftPad empty fails");
         retVal = false;
     }
 
@@ -352,37 +352,37 @@ async function testQuoteDequote() {
 
     let s = crdtuxp.dQ("");
     if (s != "\"\"") {
-        await crdtuxp.logError(arguments, "failed to crdtuxp.dQ(\"\")");
+        crdtuxp.logError(arguments, "failed to crdtuxp.dQ(\"\")");
         retVal = false;
     }
 
     s = crdtuxp.dQ('');
     if (s != "\"\"") {
-        await crdtuxp.logError(arguments, "failed to crdtuxp.dQ('')");
+        crdtuxp.logError(arguments, "failed to crdtuxp.dQ('')");
         retVal = false;
     }
 
     s = crdtuxp.sQ("");
     if (s != "''") {
-        await crdtuxp.logError(arguments, "failed to crdtuxp.sQ(\"\")");
+        crdtuxp.logError(arguments, "failed to crdtuxp.sQ(\"\")");
         retVal = false;
     }
 
     s = crdtuxp.sQ('');
     if (s != "''") {
-        await crdtuxp.logError(arguments, "failed to crdtuxp.sQ('')");
+        crdtuxp.logError(arguments, "failed to crdtuxp.sQ('')");
         retVal = false;
     }
 
     s = crdtuxp.dQ("abc'\"\x00\n\r\t\x0a\x0d\x09\u0061\u007f\x80\u0080\u0123\u07ff\u1000\u7fff");
     if (s != "\"abc'\\\"\\x00\\n\\r\\t\\n\\r\\ta\\x7f\\u0080\\u0080\\u0123\\u07ff\\u1000\\u7fff\"") {
-        await crdtuxp.logError(arguments, "failed crdtuxp.dQ complex string");
+        crdtuxp.logError(arguments, "failed crdtuxp.dQ complex string");
         retVal = false;
     }
 
     s = crdtuxp.sQ("abc'\"\x00\n\r\t\x0a\x0d\x09\u0061\u007f\x80\u0080\u0123\u07ff\u1000\u7fff");
     if (s != "'abc\\'\"\\x00\\n\\r\\t\\n\\r\\ta\\x7f\\u0080\\u0080\\u0123\\u07ff\\u1000\\u7fff'") {
-        await crdtuxp.logError(arguments, "failed crdtuxp.sQ complex string");
+        crdtuxp.logError(arguments, "failed crdtuxp.sQ complex string");
         retVal = false;
     }
 
@@ -391,13 +391,13 @@ async function testQuoteDequote() {
 
     s = crdtuxp.dQ([0x61, 0x62, 0x63, 0x27, 0x22, 0x00, 0x0A, 0x0D, 0x09, 0x61, 0x7f, 0x80, 0xFF]);
     if (s != "\"abc'\\\"\\x00\\n\\r\\ta\\x7f\\x80\\xff\"") {
-        await crdtuxp.logError(arguments, "failed crdtuxp.dQ complex byte array");
+        crdtuxp.logError(arguments, "failed crdtuxp.dQ complex byte array");
         retVal = false;
     }
 
     s = crdtuxp.sQ([0x61, 0x62, 0x63, 0x27, 0x22, 0x00, 0x0A, 0x0D, 0x09, 0x61, 0x7f, 0x80, 0xFF]);
     if (s != "'abc\\'\"\\x00\\n\\r\\ta\\x7f\\x80\\xff'") {
-        await crdtuxp.logError(arguments, "failed crdtuxp.sQ complex byte array");
+        crdtuxp.logError(arguments, "failed crdtuxp.sQ complex byte array");
         retVal = false;
     }
 
@@ -408,20 +408,20 @@ async function testQuoteDequote() {
 
     s = crdtuxp.sQ(s);
     if (s != "'\\x00\\x01\\x02\\x03\\x04\\x05\\x06\\x07\\x08\\t\\n\\x0b\\x0c\\r\\x0e\\x0f\\x10\\x11\\x12\\x13\\x14\\x15\\x16\\x17\\x18\\x19\\x1a\\x1b\\x1c\\x1d\\x1e\\x1f !\"#$%&\\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\\x7f\\x80\\x81\\x82\\x83\\x84\\x85\\x86\\x87\\x88\\x89\\x8a\\x8b\\x8c\\x8d\\x8e\\x8f\\x90\\x91\\x92\\x93\\x94\\x95\\x96\\x97\\x98\\x99\\x9a\\x9b\\x9c\\x9d\\x9e\\x9f\\xa0\\xa1\\xa2\\xa3\\xa4\\xa5\\xa6\\xa7\\xa8\\xa9\\xaa\\xab\\xac\\xad\\xae\\xaf\\xb0\\xb1\\xb2\\xb3\\xb4\\xb5\\xb6\\xb7\\xb8\\xb9\\xba\\xbb\\xbc\\xbd\\xbe\\xbf\\xc0\\xc1\\xc2\\xc3\\xc4\\xc5\\xc6\\xc7\\xc8\\xc9\\xca\\xcb\\xcc\\xcd\\xce\\xcf\\xd0\\xd1\\xd2\\xd3\\xd4\\xd5\\xd6\\xd7\\xd8\\xd9\\xda\\xdb\\xdc\\xdd\\xde\\xdf\\xe0\\xe1\\xe2\\xe3\\xe4\\xe5\\xe6\\xe7\\xe8\\xe9\\xea\\xeb\\xec\\xed\\xee\\xef\\xf0\\xf1\\xf2\\xf3\\xf4\\xf5\\xf6\\xf7\\xf8\\xf9\\xfa\\xfb\\xfc\\xfd\\xfe\\xff'") {
-        await crdtuxp.logError(arguments, "failed crdtuxp.sQ byte array with all bytes 0-255");
+        crdtuxp.logError(arguments, "failed crdtuxp.sQ byte array with all bytes 0-255");
         retVal = false;        
     }
 
     s = crdtuxp.deQuote(s);
     let isOK = true;
     if (s.length != 256) {
-        await crdtuxp.logError(arguments, "crdtuxp.deQuote wrong length");
+        crdtuxp.logError(arguments, "crdtuxp.deQuote wrong length");
         retVal = false;        
     }
     else {
         for (let idx = 0; idx < 256; idx++) {
             if (s[idx] != idx) {
-                await crdtuxp.logError(arguments, "crdtuxp.deQuote wrong byte #" + idx);
+                crdtuxp.logError(arguments, "crdtuxp.deQuote wrong byte #" + idx);
                 retVal = false;        
                 break;
             }
@@ -451,7 +451,7 @@ async function testPersistData() {
 
         let splitTimestamps = persistData.split("\t");
         if (splitTimestamps.length != 2) {
-            await crdtuxp.logError(arguments, "failed to split timestamps");
+            crdtuxp.logError(arguments, "failed to split timestamps");
             retVal = false;
             break;
         }
@@ -459,14 +459,14 @@ async function testPersistData() {
         let lastSavedTimestamp = parseInt(splitTimestamps[0]);
         let lastSavedTimestampPlus1000 = parseInt(splitTimestamps[1]);
         if (lastSavedTimestamp + 1000 != lastSavedTimestampPlus1000) {
-            await crdtuxp.logError(arguments, "failed to match timestamps");
+            crdtuxp.logError(arguments, "failed to match timestamps");
             retVal = false;
             break;
         }
 
         let secondsSinceLastRun = nowTimestamp - lastSavedTimestamp;
         if (secondsSinceLastRun < 0) {
-            await crdtuxp.logError(arguments, "invalid secondsSinceLastRun");
+            crdtuxp.logError(arguments, "invalid secondsSinceLastRun");
             retVal = false;
             break;
         }
@@ -482,23 +482,23 @@ async function testToHex() {
     let retVal = true;
 
     if (crdtuxp.toHex(10, 2) != "0a") {
-        await crdtuxp.logError(arguments, "toHex(10,2) failed");
+        crdtuxp.logError(arguments, "toHex(10,2) failed");
         retVal = false;
     }
 
     // 2-s complement
     if (crdtuxp.toHex(-10, 2) != "f6") {
-        await crdtuxp.logError(arguments, "toHex(-10,2) failed");
+        crdtuxp.logError(arguments, "toHex(-10,2) failed");
         retVal = false;
     }
 
     if (crdtuxp.toHex(65535, 2) != "ff") {
-        await crdtuxp.logError(arguments, "toHex(65535,2) failed");
+        crdtuxp.logError(arguments, "toHex(65535,2) failed");
         retVal = false;
     }
 
     if (crdtuxp.toHex(-65535, 2) != "01") {
-        await crdtuxp.logError(arguments, "toHex(-65535,2) failed");
+        crdtuxp.logError(arguments, "toHex(-65535,2) failed");
         retVal = false;
     }
 
@@ -513,7 +513,7 @@ async function testUTFRoundTrip() {
     let bytes = crdtuxp.strToUTF8(s);
     let sRoundTrip = crdtuxp.binaryUTF8ToStr(bytes);
     if (s != sRoundTrip) {
-        await crdtuxp.logError(arguments, "failed to round trip a string to UTF8 and back");
+        crdtuxp.logError(arguments, "failed to round trip a string to UTF8 and back");
         retVal = false;
     }
 
@@ -540,7 +540,7 @@ async function run() {
     let success = true;
 
     try {
-        await crdtuxp.logNote(arguments, "Starting crdtuxp_test");
+        crdtuxp.logNote(arguments, "Starting crdtuxp_test");
 
         // You need something similar to this to enable CRDT
         // await crdtuxp.setIssuer("1186cb861234567377c49d7eade","my@email.com");
@@ -552,14 +552,14 @@ async function run() {
                 let ftn = tests[idx];
                 result = await ftn();            
                 if (! result) {
-                    await crdtuxp.logError(arguments, "failed test " + ftn.name);
+                    crdtuxp.logError(arguments, "failed test " + ftn.name);
                 }
                 else {
-                    await crdtuxp.logNote(arguments, "passed test " + ftn.name);
+                    crdtuxp.logNote(arguments, "passed test " + ftn.name);
                 }
             }
             catch (err) {
-                await crdtuxp.logError(arguments, "throws " + err + " for test idx " + idx);
+                crdtuxp.logError(arguments, "throws " + err + " for test idx " + idx);
                 result = false;
             }
 
@@ -567,10 +567,10 @@ async function run() {
         }
     }
     catch (err) {
-        await crdtuxp.logError(arguments, "throws " + err);
+        crdtuxp.logError(arguments, "throws " + err);
     }
     
-    await crdtuxp.logNote(arguments, "crdtuxp_test complete");
+    crdtuxp.logNote(arguments, "crdtuxp_test complete");
 
     crdtuxp.popLogLevel();
 
